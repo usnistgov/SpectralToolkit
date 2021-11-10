050ec02a1e0682a554477b06dbe61eac165331fb
Allen Goldstein <allen.goldstein@nist.gov>
2021-11-10 12:01:02 -0500

    Added ~FM_Eng_2.~ files.  These add the grid search to Gregories CVI program.

---
+++ Sandbox/FM_Kyrizis/LabWindows/Hooke_Jeeves_Student3_Wave_Fourier_Hz_FM_Eng_2.h
@@ -0,0 +1,111 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/* Copyright (c) National Instruments 2021. All Rights Reserved.          */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  CONFIG                          1
+#define  CONFIG_CONFIGURAR               2
+#define  CONFIG_DELTAF                   3
+#define  CONFIG_FREQ_GEN                 4
+#define  CONFIG_WFM                      5
+#define  CONFIG_SIGMA                    6
+#define  CONFIG_SAMPLE_SPACING           7
+#define  CONFIG_DC_GEN_MOD               8
+#define  CONFIG_DC_GEN                   9
+#define  CONFIG_AMPL_GEN                 10
+#define  CONFIG_AMPL_GEN_MOD             11
+#define  CONFIG_FREQ_GEN_MOD             12
+#define  CONFIG_FASE_GEN                 13
+#define  CONFIG_FASE_GEN_MOD             14
+#define  CONFIG_NHARM_GEN_MOD            15
+#define  CONFIG_INO                      16
+
+#define  GRAFICOS                        2
+#define  GRAFICOS_YMAX_RES               2
+#define  GRAFICOS_YMIN_RES               3
+#define  GRAFICOS_XMAX_RES               4
+#define  GRAFICOS_XMIN_RES               5
+#define  GRAFICOS_GRAPH_RESIDUE          6
+#define  GRAFICOS_ESCALA_DADOS           7
+#define  GRAFICOS_DADOS                  8
+#define  GRAFICOS_YMAX                   9
+#define  GRAFICOS_YMIN                   10
+#define  GRAFICOS_GRAPH_SIGNAL           11
+#define  GRAFICOS_XMAX                   12
+#define  GRAFICOS_XMIN                   13
+#define  GRAFICOS_GRAPH_DATA             14
+
+#define  INCERT                          3
+#define  INCERT_ARQUIVAR                 2
+#define  INCERT_SIGMA_ERR                3
+#define  INCERT_SIGMA                    4
+#define  INCERT_LER                      5
+#define  INCERT_COVAR_FREQ               6
+#define  INCERT_COVAR_AMP                7
+#define  INCERT_LED                      8
+
+#define  MEDICAO                         4
+#define  MEDICAO_STARTPT_1               2
+#define  MEDICAO_ARQUIVAR                3
+#define  MEDICAO_ENDPT_1                 4
+#define  MEDICAO_FUNDAMENTAL             5
+#define  MEDICAO_FUNDAMENTAL_NLS         6
+#define  MEDICAO_THD                     7
+#define  MEDICAO_LED                     8
+#define  MEDICAO_ACRMS_NLS               9
+#define  MEDICAO_NSTLOGE                 10
+#define  MEDICAO_ENDPT_NLS               11
+#define  MEDICAO_ENDPT_2                 12
+#define  MEDICAO_STARTPT_2               13
+#define  MEDICAO_STARTPT_NLS             14
+#define  MEDICAO_ENDPT_0                 15
+#define  MEDICAO_STARTPT_0               16
+#define  MEDICAO_ITERS                   17
+#define  MEDICAO_IMAX_NLS                18
+#define  MEDICAO_EPSMIN_NLS              19
+#define  MEDICAO_RHO_BEGIN_NLS           20
+#define  MEDICAO_IMAX                    21
+#define  MEDICAO_EPSMIN                  22
+#define  MEDICAO_RHO_BEGIN               23
+#define  MEDICAO_ID                      24
+#define  MEDICAO_NVARS                   25
+#define  MEDICAO_FREQ_NLS                26
+#define  MEDICAO_MEDIR_FLICKER           27
+#define  MEDICAO_FREQ                    28
+#define  MEDICAO_AMPL_NLS                29
+#define  MEDICAO_AMPL                    30
+#define  MEDICAO_MEDIR                   31
+#define  MEDICAO_LIST_FASE               32
+#define  MEDICAO_LIST_FREQ               33
+#define  MEDICAO_LIST_AMP                34
+
+#define  SYSPANEL                        5
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+#define  MENU                            1
+#define  MENU_CONFIGURACAO               2
+#define  MENU_MEDICAO                    3
+#define  MENU_INCERT                     4
+#define  MENU_GRAFICOS                   5
+#define  MENU_SAIR                       6
+
+
+     /* (no callbacks specified in the resource file) */ 
+
+
+#ifdef __cplusplus
+    }
+#endif
